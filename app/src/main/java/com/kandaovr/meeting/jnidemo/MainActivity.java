8fb7abc07fb21adaef313a994c8b40209321be80
cjx <1153448695@qq.com>
2022-05-15 20:42:59 +0800

    git test

--- app/src/main/java/com/kandaovr/meeting/jnidemo/MainActivity.java
+++ app/src/main/java/com/kandaovr/meeting/jnidemo/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("common");
     }
 
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
